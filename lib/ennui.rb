e37493b5445bf188e10366edbfc5d00f253c3b69
toddmohney <toddmohney@gmail.com>
2013-04-16 23:32:09 -0400

    Add who_cares? method. Add rspec helper method

--- lib/ennui.rb
+++ lib/ennui.rb
@@ -1,8 +1,13 @@
 module Ennui
 
-  def Ennui.sometimes
+  def Ennui.sometimes?
     rand = Random.new.rand(0..1000)
     rand % 2 == 0
+  end
+
+  def Ennui.who_cares?
+    rand = Random.new.rand(0..1000)
+    rand % 4 == 0
   end
 
 end
--- spec/lib/ennui_spec.rb
+++ spec/lib/ennui_spec.rb
@@ -3,18 +3,39 @@ require 'pry'
 
 describe Ennui do
 
-  describe "#sometimes" do
+  describe "#sometimes?" do
     before do
-      @true_count = 0
+      @truthiness_score = 0
 
       100.times do
-        @true_count += 1 if Ennui.sometimes
+        @truthiness_score += 1 if Ennui.sometimes?
       end
+
+      puts "Truthiness score: #{@truthiness_score}"
     end
 
-    it "is true sometimes" do
-      @true_count.should be_within(10).of(50)
+    it "is true about %50 of the time" do
+      score = truthiness_score(1000, Proc.new { Ennui.sometimes? })
+      score.should be_within(0.10).of(0.50)
     end
+  end
+
+  describe "#who_cares?" do
+    it "is true about %25 of the time" do
+      score = truthiness_score(1000, Proc.new { Ennui.who_cares? })
+      score.should be_within(0.10).of(0.25)
+    end
+  end
+
+  def truthiness_score(iterations, proc)
+    truthiness = 0
+    iterations.times do
+      truthiness += 1 if proc.call
+    end
+
+    score = truthiness.to_f / iterations.to_f
+    puts "Truthiness score: #{score}"
+    score
   end
 
 end
