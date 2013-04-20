a7482c4fe97d50326bd4e593b6ee8e99476221b1
toddmohney <toddmohney@gmail.com>
2013-04-20 10:57:44 -0400

    Update usage examples

--- examples/usage.rb
+++ examples/usage.rb
@@ -5,11 +5,28 @@ require "ennui"
 class Example 
   include Ennui
 
-  def initialize
+  def do_sometimes_predicate_example
+    puts
+    puts "do sometimes predicate example"
+    puts "------------------------------"
     10.times do
       puts "meh" if sometimes?
     end
   end
+
+
+  def do_sometimes_block_example
+    puts
+    puts "do sometimes block example"
+    puts "--------------------------"
+    10.times do
+      sometimes do
+        puts "pfft"
+      end
+    end
+  end
 end
 
-Example.new
+example_runner = Example.new
+example_runner.do_sometimes_predicate_example
+example_runner.do_sometimes_block_example
