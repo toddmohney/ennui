66fda44f009a7b9d6d2d806346b1dd2259d75cf8
toddmohney <toddmohney@gmail.com>
2013-04-20 10:59:50 -0400

    Move truthiness_score to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,15 @@ require 'simplecov'
 SimpleCov.start
 
 require 'ennui'
+
+def truthiness_score(iterations, proc)
+  truthiness = 0
+  iterations.times do
+    truthiness += 1 if proc.call
+  end
+
+  score = truthiness.to_f / iterations.to_f
+  puts "Truthiness score: #{score}"
+  score
+end
+
